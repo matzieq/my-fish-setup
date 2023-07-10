623e23e193d85e99cada248e89e1aeb5ee466c4f
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2023-07-10 13:43:49 +0200

    Initial commit

---
+++ config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    set -g theme_color_scheme gruvbox
+    fish_add_path $HOME/bin/
+    alias love="/Applications/love.app/Contents/MacOS/love"
+    alias hajda="git push -u origin HEAD"
+    alias twchoo="timew start Chooose"
+    alias twstop="timew stop"
+    alias twlun="timew start lunch"
+    alias twmov="timew move"
+    alias twmod="timew modify end"
+    alias twsum="timew summary :ids"
+    alias twint="timew start internal"
+    alias vim="nvim"
+    alias startpsg="tmux && tmux send-keys 'cd ~/dev/work/chooose/landing-page-generator Enter nvim . Enter C-b C-w'"
+    eval "$(pyenv init -)"
+end
+
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/maciekkozlowski/.ghcup/bin $PATH # ghcup-env
---
+++ functions/la.fish
@@ -0,0 +1,3 @@
+function la --wraps='exa -la' --description 'alias la exa -la'
+  exa -la $argv; 
+end
